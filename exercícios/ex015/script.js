fa7fb6e0715043725e50ccbb54a75920317d022c
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-09-22 12:56:50 -0300

    ..

---
+++ exercícios/ex015/script.js
@@ -0,0 +1,21 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('iano')
+    var res = document.getElementById('res')
+
+    if(fano.value.length == 0 || fano.value > ano){
+        window.alert('Verifique os dados e tente novamente!')
+    }else{
+       var fsex = document.getElementsByName('genero')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       if(fsex[0].checked){
+          genero = 'Homem'
+       }else if (fsex[1].checked){
+          genero = 'Mulher'
+       }
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+
+    }
+}
