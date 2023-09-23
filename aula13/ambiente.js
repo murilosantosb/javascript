6801da40c8689fc819f58e2c9f510a19df506229
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-09-23 20:24:51 -0300

    terminei aula 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+while (c <= 5){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+
+
+
+
+
+
+/*
+var c = 1
+while (c <= 5){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') */
