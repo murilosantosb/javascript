19d1a96c350557cf6d9050e78c32cc294b4237d8
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-09-29 18:59:37 -0300

    .

--- exercícios/ex017/script.js
+++ exercícios/ex017/script.js
@@ -1,20 +1,17 @@
 function tabuada(){
     let num = document.getElementById('inum')
     let tab = document.getElementById('seltab')
-    
-    if(num.value.length == 0 ){
-        alert('Você não digitou nenhum número!')
-        
+
+    if(num.value.length ==0 ){
+        alert('Você não digitou nada!')
     }else{
         let n = Number(num.value)
         let c = 1
-        tab.innerHTML = ''
-        while (c <= 10){
-            let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n * c}`
-            tab.appendChild(item)
-            c++
+        while(c <= 10){
+           let item = document.createElement('option') // Adicionado um Elemento filho com JS
+           item.text = `${n} x ${c} = ${n * c}`
+           tab.appendChild(item) // Adicionando creatElement dentro da tabuada
+           c++
         }
     }
-    
 }
