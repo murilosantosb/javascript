51e37d3dd2edbd02f749ef2356c439f553e37ee2
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-09-30 16:46:46 -0300

    .

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,6 +1,10 @@
-let num = [5, 2 ,3, 6, 9]
-num.push(1)
-num.sort()
-console.log(num)
-console.log(`Nosso vetor tem ${num.length} elementos`)
-console.log(`O primeiro valor do vetor é ${num[0]}`)
+let num = [5, 2 ,3, 6, 9, 8, 1, 3, 4, 7]
+
+//num.sort()
+/*for(let pos = 0; pos < num.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+} */
+
+for(let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
---
+++ aula15/ambiente02.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 9, 7]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(5)
+console.log(`O valor 5 está na posição ${pos}`)
