ebbb16b98d93e985c407ec5e619f8e6d8977b109
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-10-01 00:10:36 -0300

    atualizei

---
+++ aula16/funcao02.js
@@ -0,0 +1,8 @@
+function soma(n1=0 , n2=0){  // n1=0 , Seu eu não colocar valores na chamada ele vai valer 0
+    return n1 + n2
+}
+
+console.log(soma(7))  // N1 + N2 
+
+/* Ser o valor da ação tiver faltando com a do parâmetro o valor vai ser NaN
+Só colocar =0 que não da problema  */
---
+++ exercícios/ex018/script.js
@@ -0,0 +1,6 @@
+function adicionar(){
+    let n = [1,2,3,4]
+    for(let c = n; c <= 100; c++){
+        return n
+    }
+}
