71a79c32a664908b82a707e3d33bc7b9d987c3b5
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-10-16 13:00:02 -0300

    .

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,5 +1,5 @@
-var agora = new Date()
-var diaSem = agora.getDay()
+var agora = new Date();
+var diaSem = agora.getDay();
 
 
 /*
@@ -11,9 +11,35 @@ var diaSem = agora.getDay()
    5 = Sexta
    6 = Sábado
 */ 
-console.log(diaSem)
 
-switch(diaSem){
+switch (diaSem) {
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Segunda');
+        break;
+    case 2:
+        console.log('Terça');
+        break;
+    case 3:
+        console.log('Quarta');
+        break;
+    case 4:
+        console.log('Quinta');
+        break;
+    case 5:
+        console.log('Sexta');
+        break;
+    case 6:
+        console.log('Sábado');
+        break;
+    default:
+        console.log("[ERRO] Dia inválido!");
+}
+
+
+/*switch(diaSem){
     case 0:
         console.log('Domingo')
         break
@@ -35,7 +61,4 @@ switch(diaSem){
     case 6:
         console.log('Sábado') 
         break
-    default:
-        console.log(`[ERRO] Dia inválido!`)           
-                        
-}
+    */
