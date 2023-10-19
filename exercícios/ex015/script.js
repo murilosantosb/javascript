616e53a1e24a9956630fcf51b08d3ed43ad7d5fd
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-10-19 09:46:54 -0300

    .

--- exercícios/ex015/script.js
+++ exercícios/ex015/script.js
@@ -1,47 +1,53 @@
 function verificar(){
-  let data = new Date()
+  let data = new Date
   let ano = data.getFullYear()
   let fano = document.getElementById('iano')
   let res = document.getElementById('res')
 
   if(fano.value.length == 0 || fano.value > ano){
-    alert('[ERRO] Verifique os Dados a seguir')
+    alert('Verifique e tente novamente!')
   }else{
     let fsex = document.getElementsByName('genero')
     let idade = ano - Number(fano.value)
     let genero = ''
     let img = document.createElement('img')
-    img.setAttribute('id', 'foto') // É a mesma coisa de ir no HTML e criar um id='foto'
-  
+    img.setAttribute('id','foto')
 
-  if(fsex[0].checked){
-    genero = 'Homem'
-    if(idade >=0 && idade < 10){
-      //Criança
-      img.setAttribute('src', 'imagens/foto-menino.jpg')
-    }else if (idade < 21){
-      //Jovem
+    if(fsex[0].checked){
+      genero = 'Homem'
+      if(idade >=0 && idade < 10){
+      img.setAttribute('src','imagens/foto-menino.jpg')
+      res.innerHTML = `Identificamos um menino com ${idade} anos`
+    }else if(idade < 21){
       img.setAttribute('src', 'imagens/foto-adolecentem.jpg')
-    }else if (idade < 50){
-      //Adulto
-      img.setAttribute('src', 'imagens/foto-homem.jpg')
+      res.innerHTML = `Identificamos um adolecente com ${idade} anos`
+    }else if(idade < 55){
+       img.setAttribute('src', 'imagens/foto-homem.jpg')
+       res.innerHTML = `Identificamos um homem com ${idade} anos`
     }else{
-      //Idoso
-      img.setAttribute('src', 'imagens/foto-idoso.jpg')
+      img.setAttribute('src','imagens/foto-idoso.jpg')
+      res.innerHTML = `Identificamos um idoso com ${idade} anos`
     }
 
-  }else if(fsex[1].checked){
+
+  } else if(fsex[1].checked){
     genero = 'Mulher'
-    if(idade >=0 && idade < 10){
+    if(idade >= 0 && idade <10){
       img.setAttribute('src','imagens/foto-menina.jpg')
+      res.innerHTML = `Identificamos uma menina com ${idade} anos`
     }else if(idade < 21){
-      img.setAttribute('src', 'imagens/foto-adolecentef.jpg')
-    }else if(idade < 50){
-      img.setAttribute('src', 'imagens/foto-mulher.jpg')
+      img.setAttribute('src','imagens/foto-adolecentef.jpg')
+      res.innerHTML = `Identificamos uma adolecente com ${idade} anos`
+    }else if(idade < 55){
+      img.setAttribute('src','imagens/foto-mulher.jpg')
+      res.innerHTML = `Identificamos uma mulher com ${idade} anos`
     }else{
-      img.setAttribute('src', 'imagens/foto-idosa.jpg')
+      img.setAttribute('src','imagens/foto-idosa.jpg')
+      res.innerHTML = `Identificamos uma idosa com ${idade} anos`
     }
   }
-  res.innerHTML = `Detectamos ${genero} com ${idade} anos`
-       res.appendChild(img)
-}} 
+  
+ // res.innerHTML = `Identificamos ${genero} com ${idade} anos`
+  res.appendChild(img)
+
+} } 
