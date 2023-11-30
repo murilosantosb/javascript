2e4da0cacddad472fd5cafcf39b011335640324c
Murilo dos Santos <142051797+murilosantosb@users.noreply.github.com>
2023-11-30 14:21:56 -0300

    .

--- Revisão/Array/CarrinhoDeCompras/js/produtos.js
+++ Revisão/Array/CarrinhoDeCompras/js/produtos.js
@@ -4,61 +4,66 @@ const todoProdutos = document.querySelector('#todoProdutos')
 
 
 
-
 const divNova = document.createElement('div')
 divNova.setAttribute('id','todoProdutos')
 divNova.innerHTML = `
 <div class="Produto">
 <img src="./img/product-1.jpg" alt="">
-<p >Camiseta Branca</p>
-<p>79,99R$</p>
+<p >Camisa Larga com Bolsos</p>
+<p>70,00R$</p>
 <span class="material-symbols-outlined" id="adicionar_ao_carrinho">  shopping_cart </span>
 </div>
 
 <div>
 <img src="./img/product-2.jpg" alt="">
-<p>Camiseta Branca</p>
-<p>79,99R$</p>
-<span class="material-symbols-outlined"  id="adicionar_ao_carrinho">  shopping_cart </span>
+<p>Casaco Reto com Lã</p>
+<p>185,00R$</p>
+<span class="material-symbols-outlined s"  id="adicionar_ao_carrinho">  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-3.jpg" alt="">
-<p>Camiseta Branca</p>
+<p>Jaqueta com Efeito</p>
 <p>79,99R$</p>
 <span class="material-symbols-outlined" id="adicionar_ao_carrinho" >  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-4.jpg" alt="">
-<p>Camiseta Branca</p>
-<p>79,99R$</p>
+<p>Sobretudo em Mescla de Lã</p>
+<p>159,99R$</p>
 <span class="material-symbols-outlined" id="adicionar_ao_carrinho" >  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-5.jpg" alt="">
-<p>Camiseta Branca</p>
+<p>Camisa Larga Acolchoada de Veludo</p>
 <p>79,99R$</p>
 <span class="material-symbols-outlined"  id="adicionar_ao_carrinho">  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-6.jpg" alt="">
-<p>Camiseta Branca</p>
-<p>79,99R$</p>
+<p>Camiseta Casaco de Lã com Boltões</p>
+<p>199,99R$</p>
 <span class="material-symbols-outlined" id="adicionar_ao_carrinho" >  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-7.jpg" alt="">
-<p>Camiseta Branca</p>
+<p>Casaco Fino</p>
 <p>79,99R$</p>
 <span class="material-symbols-outlined" id="adicionar_ao_carrinho" >  shopping_cart </span>
 </div>
 <div>
 <img src="./img/product-8.jpg" alt="">
-<p>Camiseta Branca</p>
-<p>79,99R$</p>
+<p>Casaco</p>
+<p>149,99R$</p>
 <span class="material-symbols-outlined"  id="adicionar_ao_carrinho">  shopping_cart </span>`
 
 Selecao_Principal.appendChild(divNova)
 
+const btn_carinho = [...document.querySelectorAll('#adicionar_ao_carrinho')]
 
+btn_carinho.map((el)=>{
+   el.addEventListener('click',(evt)=>{
+    alert('ddd')
+   })
+})
 
 
